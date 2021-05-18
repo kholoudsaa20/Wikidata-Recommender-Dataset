2f23626388e069d35eb15b1450d917d59274d7a6
Kholoud Alghamdi <kholoud.alghamdi@kcl.ac.uk>
2021-05-18 21:02:56 +0100

    Add the MF script to the script folder

--- Scripts/MF.py
+++ Scripts/MF.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c75ad568123de4155206bab1a5b75a2a862e95a9669629b24831edc3803825a3
-size 7487
+oid sha256:7ca32b3de7beecfe8c04df2e0feff90bc27610a0643b8b4d45d7fb2d9f0f2e52
+size 6791
