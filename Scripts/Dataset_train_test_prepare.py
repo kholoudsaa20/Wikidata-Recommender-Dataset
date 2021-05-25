7221bd4b7495a65fb244de7e1303bd62e5f3df6d
Kholoud Saad <kholoud.alghamdi@kcl.ac.uk>
2021-05-25 20:28:00 +0100

    Add Dataset prepare script

--- Scripts/Dataset_train_test_prepare.py
+++ Scripts/Dataset_train_test_prepare.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8bf26ef8e50f6ad9ea9f5117cfc026f054fada379ca2aba3c77a5c66fc19a8e
-size 1801
+oid sha256:8cd948e430fbc2e577eeb3a7575c162ae3f30f2c19060d51b7f85521d2a7500e
+size 3511
